eed3f44336d0aa353b000780a52c230bfcaea60e
Cansu Baydar <cansu.baydar@gmail.com>
2022-03-21 00:48:50 +0300

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 ||  
+        typeof num1 !== 'number' ||  
+        typeof num2 !== 'number') 
+        return 'ERROR';
 
+    let sum = 0;
+    let j, k;
+
+    (num1 > num2) ? (j = num2, k = num1) : (j = num1, k = num2);
+
+    for(let i = j; i <= k; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
