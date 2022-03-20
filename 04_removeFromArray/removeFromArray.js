608fed8c81da4619bff393b378e98ad1f953f7db
Cansu Baydar <cansu.baydar@gmail.com>
2022-03-20 18:28:27 +0300

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a, ...arr) {
+    return a.filter(value => !arr.includes(value));
+    
+    // Second solution
+    // return a.filter(value => arr.indexOf(value) == -1);
 };
 
 // Do not edit below this line
