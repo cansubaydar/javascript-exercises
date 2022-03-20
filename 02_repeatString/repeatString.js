ab1c2788d26b2354ac581d895f83f6d406518e96
Cansu Baydar <cansu.baydar@gmail.com>
2022-03-20 16:46:09 +0300

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0) {
+        return 'ERROR';
+    }
 
+    let string = '';
+    for (let i = 1; i <= num; i++) {
+        string += str;
+    }
+    return string;
+
+    // Using the repeat() method
+    // return num < 0 ? 'ERROR' : str.repeat(num);
 };
 
 // Do not edit below this line
