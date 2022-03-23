8d0f2f31db89d5cd5d57eb913b10c71223c506c2
Cansu Baydar <cansu.baydar@gmail.com>
2022-03-23 13:42:02 +0300

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(F) {
+  return Math.round(((F - 32) * 5 / 9) * 10) / 10;
+  // return parseFloat(((F - 32) * 5 / 9).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(C) {
+  return Math.round(((C * 9 / 5) + 32) * 10) / 10;
+  // return parseFloat(((C * 9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
