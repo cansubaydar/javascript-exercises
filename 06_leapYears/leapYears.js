ae136d91ac7049834173c033ddb584ff4116121e
Cansu Baydar <cansu.baydar@gmail.com>
2022-03-23 12:37:24 +0300

    Complete leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(y) {
+    return (y % 4 === 0) && !(y % 100 === 0 && y % 400 !== 0);
 
+    // if(y % 4 === 0) {
+    //     if(y % 100 === 0 && y % 400 !== 0) {
+    //         return false;
+    //     } else {
+    //         return true;
+    //     }
+    // } else {
+    //     return false;
+    // }
 };
 
 // Do not edit below this line
