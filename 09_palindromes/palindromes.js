06fbe0f80d0f5a3df9961addb5b3859578124632
Cansu Baydar <cansu.baydar@gmail.com>
2022-03-23 22:02:42 +0300

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const words = str.replace(/[^a-z]+/gi, '').toLowerCase();
+    const reverseWord = words.split('').reverse().join('');
 
+    return (words === reverseWord);
 };
 
 // Do not edit below this line
