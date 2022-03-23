7dbb2f87c125f3736b5059474e80db1e816c4dfb
Cansu Baydar <cansu.baydar@gmail.com>
2022-03-24 01:46:27 +0300

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort((a,b) => {
+        const currentYear = new Date().getFullYear();
+        const ageA = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        const ageB = (b.yearOfDeath || currentYear) - b.yearOfBirth;
 
+        return ageA > ageB ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
